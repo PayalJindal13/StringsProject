94087915307b15a58a00242ce5d36ce8afad2736
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-11 00:18:20 +0530

    Some Changes done

--- string2.js
+++ string2.js
@@ -5,7 +5,8 @@ module.exports = function IPAddressToArray(ipv4Address){
 
         // Returning array for valid ipv4 address
         if (ipv4Array.length === 4){
-            if (ipv4Array.every(element => {return (!isNaN(Number(element)) && Number(element) <= 255 && Number(element) >=0)})){
+            // checking range of address from 0-255 
+            if (ipv4Array.every(element => {return (Number(element) <= 255 && Number(element) >=0)})){
                 let finalArray = ipv4Array.map(num => {return Number(num)});
                 return finalArray;
             }
--- string5.js
+++ string5.js
@@ -1,5 +1,5 @@
 module.exports = function arrayToString(arr){
-    // returning string from aa valid array
+    // returning string from a valid array
     if(arr){
         // check for empty array
         if (arr.length === 0) return ' ';
